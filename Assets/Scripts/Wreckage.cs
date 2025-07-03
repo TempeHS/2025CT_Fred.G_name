86635aa3d44249b6da7c880a3670277de67e98f3
fredg78 <frederic.garran@education.nsw.gov.au>
2025-07-03 15:05:03 +1000

    .

--- Assets/Scripts/Wreckage.cs
+++ Assets/Scripts/Wreckage.cs
@@ -7,12 +7,17 @@ public class Wreckage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            Destroy(gameObject);
     }
 }
